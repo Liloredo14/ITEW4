263fdf0aea4c8c49e3874bc525e141edaf1f005d
Liam Loredo <liams.loredo@gmail.com>
2024-06-07 11:44:50 +0800

    updated

--- industry/src/App.js
+++ industry/src/App.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/free-solid-svg-icons';
-import '@fortawesome/free-brands-svg-icons'
+import '@fortawesome/free-brands-svg-icons';
 import './App.css';
 
 import AppHeader from './components/header';
@@ -8,6 +8,12 @@ import AppHero from './components/hero';
 import AppAbout from './components/about';
 import AppServices from './components/services';
 import AppWorks from './components/works';
+import AppTeams from './components/teams';
+import AppTestimonials from './components/testimonials';
+import AppPricing from './components/pricing';
+import AppBlog from './components/blog';
+import AppContact from './components/contact';
+import AppFooter from './components/footer';
 
 function App() {
   return (
@@ -20,7 +26,15 @@ function App() {
         <AppAbout/>
         <AppServices/>
         <AppWorks/>
+        <AppTeams/>
+        <AppTestimonials/>
+        <AppPricing/>
+        <AppBlog/>
+        <AppContact/>
       </main>
+      <footer id='footer'>
+        <AppFooter/>
+      </footer>
     </div>
   );
 }
